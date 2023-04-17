a59cff0bbad24635ce0e7dc73805e784d2215c72
sipsena <zukasarturas@gmail.com>
2023-04-17 21:26:44 +0300

    prideta naujo projekto konfiguracija

---
+++ src/main/java/com/spring/skaiciuotuvas/InternetinisSkaiciuotuvasApplication.java
@@ -0,0 +1,13 @@
+package com.spring.skaiciuotuvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetinisSkaiciuotuvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetinisSkaiciuotuvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/skaiciuotuvas/InternetinisSkaiciuotuvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.skaiciuotuvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetinisSkaiciuotuvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
